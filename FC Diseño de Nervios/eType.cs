6eb5f1b3967a94b7537fd95db67aee16d543aa95
jrosalesmeza <51757560+jrosalesmeza@users.noreply.github.com>
2020-03-10 08:12:45 -0500

    Diagrama de Clases

---
+++ FC Diseño de Nervios/eType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FC_Diseño_de_Nervios
+{
+    public enum eType
+    {
+        Colum,
+        Beam,
+        Floor,
+        Wall
+    }
+}
